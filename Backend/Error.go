40759c802d3bec227490870474837fb1d784471c
Tnji <tichagodwill1@gmail.com>
2024-05-12 20:40:14 +0300

    Handling errors and css refactoring

--- Backend/Error.go
+++ Backend/Error.go
@@ -1,21 +1,64 @@
 package forum
 
 import (
-	"html/template"
-	"net/http"
+    "net/http"
+    // "log"
+    // "os"
 )
 
-// HandleBadRequest handles bad request errors
+// Function to serve error files
+func ServeErrorFiles() {
+    // Print current working directory
+    // cwd, err := os.Getwd()
+    // if err != nil {
+    //     log.Println("Error getting current working directory:", err)
+    // } else {
+    //     log.Println("Current working directory:", cwd)
+    // }
+
+    // Serve error files from the "Error" directory
+    fs := http.FileServer(http.Dir("Error"))
+
+    // Route requests to error files under the "/error/" URL prefix
+    http.Handle("/error/", http.StripPrefix("/error/", fs))
+}
+
+// Function to handle errors by rendering appropriate error pages
+func ErrorHandler(w http.ResponseWriter, r *http.Request, statusCode int, errorMsg string) {
+    switch statusCode {
+    case http.StatusBadRequest:
+        http.ServeFile(w, r, "Error/error400.html")
+    case http.StatusNotFound:
+        http.ServeFile(w, r, "Error/error404.html") 
+    case http.StatusInternalServerError:
+        http.ServeFile(w, r, "Error/error500.html") 
+    default:
+        w.Write([]byte(errorMsg))
+    }
+}
+
+// Function to handle bad request errors
 func HandleBadRequest(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusBadRequest)
-	tmpl, err := template.ParseFiles("../Error/400-error.html")
-	if err != nil {
-		http.Error(w, "Bad request", http.StatusInternalServerError)
-		return
-	}
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
+    ErrorHandler(w, r, http.StatusBadRequest, "Bad Request")
+}
+
+// Function to handle not found errors
+func HandleNotFound(w http.ResponseWriter, r *http.Request) {
+    // log.Println("Handling not found error") 
+    ErrorHandler(w, r, http.StatusNotFound, "Page Not Found")
+}
+
+// Function to handle internal server errors
+func HandleInternalServerError(w http.ResponseWriter, r *http.Request) {
+    ErrorHandler(w, r, http.StatusInternalServerError, "Internal Server Error")
+}
+
+func ServeStaticFiles() {
+    fs := http.FileServer(http.Dir("Error"))
+    http.Handle("/Error/", http.StripPrefix("/Error/", fs))
+}
+
+func init() {
+    ServeErrorFiles()
+    ServeStaticFiles()
 }
--- main.go
+++ main.go
@@ -1,35 +1,48 @@
 package main
+
 import (
-	forum "forum/Backend"
-	_ "github.com/mattn/go-sqlite3"
-	"log"
-	"net/http"
+    "forum/Backend" // Assuming your forum package is in a directory named "Backend"
+    "log"
+    "net/http"
 )
+
 func main() {
-	fs := http.FileServer(http.Dir("Style"))
-	http.Handle("/Style/", http.StripPrefix("/Style/", fs))
-	posts := http.FileServer(http.Dir("Posts"))
-	http.Handle("/Posts/", http.StripPrefix("/Posts/", posts))
-	profileImages := http.FileServer(http.Dir("ProfileImages"))
-	http.Handle("/ProfileImages/", http.StripPrefix("/ProfileImages/", profileImages))
-	http.HandleFunc("/", forum.Login)
-	http.HandleFunc("/SignUp", forum.SignUpHandler)
-	http.HandleFunc("/LogOut", forum.LogoutHandler)
-	http.HandleFunc("/HomePage", forum.AuthMiddleware(forum.HomeHandler))
-	http.HandleFunc("/CreatePost",forum.AuthMiddleware(forum.CreatePostHandler))
-	http.HandleFunc("/Profile", forum.AuthMiddleware(forum.ProfileHandler))
-	http.HandleFunc("/CommentHandler", forum.CommentHandler)
-	http.HandleFunc("/ProfileImageHandler", forum.ProfileImageHandler)
-	http.HandleFunc("/CommentLikeHandle", forum.CommentLikeHandle)
-	http.HandleFunc("/googleSignIn", forum.GoogleSignInHandler)
-	http.HandleFunc("/HandleBadRequest", forum.HandleBadRequest)
-	
-	/*http.HandleFunc("/", forum.Login)
-	http.HandleFunc("/SignUp", forum.SignUpHandler)
-	http.HandleFunc("/HomePage", forum.HomeHandler)
-	http.HandleFunc("/CreatePost", forum.CreatePostHandler)
-	http.HandleFunc("/Profile", forum.ProfileHandler)*/
-	forum.CreateTables()
-	log.Println("Server started on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    fs := http.FileServer(http.Dir("Style"))
+    http.Handle("/Style/", http.StripPrefix("/Style/", fs))
+
+    posts := http.FileServer(http.Dir("Posts"))
+    http.Handle("/Posts/", http.StripPrefix("/Posts/", posts))
+
+    profileImages := http.FileServer(http.Dir("ProfileImages"))
+    http.Handle("/ProfileImages/", http.StripPrefix("/ProfileImages/", profileImages))
+
+    // Registering route handlers
+    http.HandleFunc("/SignUp", forum.SignUpHandler)
+    http.HandleFunc("/LogOut", forum.LogoutHandler)
+    http.HandleFunc("/HomePage", forum.AuthMiddleware(forum.HomeHandler))
+    http.HandleFunc("/CreatePost", forum.AuthMiddleware(forum.CreatePostHandler))
+    http.HandleFunc("/Profile", forum.AuthMiddleware(forum.ProfileHandler))
+    http.HandleFunc("/CommentHandler", forum.CommentHandler)
+    http.HandleFunc("/ProfileImageHandler", forum.ProfileImageHandler)
+    http.HandleFunc("/CommentLikeHandle", forum.CommentLikeHandle)
+    http.HandleFunc("/googleSignIn", forum.GoogleSignInHandler)
+    // Registering error handlers
+    http.HandleFunc("/HandleBadRequest", forum.HandleBadRequest)
+    http.HandleFunc("/HandleNotFound", forum.HandleNotFound)
+    http.HandleFunc("/HandleInternalServerError", forum.HandleInternalServerError)
+
+    // Catch-all route handler for undefined routes
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        if r.URL.Path != "/" {
+            forum.HandleNotFound(w, r)
+            return
+        }
+        forum.Login(w, r)
+    })
+
+    // Create tables if needed
+    forum.CreateTables()
+
+    log.Println("Server started on http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
